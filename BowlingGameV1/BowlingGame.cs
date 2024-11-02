054e6553e3da37ec3272fd4d1f81f5136b3ee217
MiyamotoAkira <yefo.akira@twoormore.eu>
2024-11-02 15:40:49 +0000

    Simple version fo the Bowling kata completed

---
+++ BowlingGameV1/BowlingGame.cs
@@ -0,0 +1,56 @@
+﻿
+using Microsoft.VisualBasic;
+
+namespace BowlingGameV1;
+
+public class BowlingGame {
+    IList<int> rolls;
+    public BowlingGame() {
+        rolls = new List<int>();
+    }
+
+    public void Roll(int pins)
+    {
+        rolls.Add(pins);
+    }
+
+    public int Score()
+    {
+        var total = 0;
+        var newFrame = true;
+        var currentFrame = 1;
+        foreach (var(roll, index) in rolls.Select((f, idx)=> (f,idx))) {
+            if (currentFrame > 10) {
+                break;
+            }
+
+            total += roll;
+
+            // We handle strikes
+            if (roll == 10) {
+                if (index + 1 < rolls.Count) {
+                total += rolls[index + 1];
+                }
+                if (index + 2 < rolls.Count) {
+                    total += rolls[index + 2];
+                }
+                newFrame = true;
+                currentFrame++;
+            } else {
+                // We handle spares
+                if (!newFrame) {
+                    if (roll + rolls[index -1] == 10) {
+                        if (index + 1 < rolls.Count) {
+                            total += rolls[index+1];
+                        }
+                    }
+                }
+                newFrame = !newFrame;
+                if (newFrame) {
+                    currentFrame++;
+                }
+            }
+        }
+        return  total;
+    }
+}
