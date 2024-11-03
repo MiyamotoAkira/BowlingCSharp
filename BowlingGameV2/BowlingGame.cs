213a3706e2f62ce2e28c7f267217e2bd87e68217
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2024-11-03 11:29:46 +0000

    V2  of the bowling game

---
+++ BowlingGameV2/BowlingGame.cs
@@ -0,0 +1,112 @@
+﻿
+namespace BowlingGameV2;
+
+public class Frame
+{
+    int FirstRoll;
+    int? SecondRoll;
+    IList<int> BonusRoll;
+
+    public Frame(int firstRoll)
+    {
+        FirstRoll = firstRoll;
+        BonusRoll = new List<int>();
+    }
+
+    public int Score()
+    {
+        return FirstRoll + SecondRoll.GetValueOrDefault()+ BonusRoll.Aggregate(0, (total, bonus) => total + bonus);
+    }
+
+    public bool IsCompleted()
+    {
+        return IsStrike() || SecondRoll.HasValue;
+    }
+
+    public void AddSecondRoll(int pins)
+    {
+        SecondRoll = pins;
+    }
+
+    internal bool IsStrike()
+    {
+        return FirstRoll == 10;
+    }
+
+    internal void AddBonusRoll(int pins)
+    {
+        if (BonusRoll.Count < 2) {
+            BonusRoll.Add(pins);
+        }
+    }
+
+    internal bool IsSpare()
+    {
+        return !IsStrike() && (FirstRoll+SecondRoll == 10);
+    }
+}
+public class BowlingGame
+{
+    IList<Frame> frames;
+    public BowlingGame()
+    {
+        frames = new List<Frame>();
+    }
+
+    public void Roll(int pins)
+    {
+        if (frames.Count == 10 ) {
+            frames.Last().AddBonusRoll(pins);
+            if (frames[frames.Count -2].IsStrike()) {
+                frames[frames.Count -2].AddBonusRoll(pins);
+            }
+            return;
+        }
+
+        if (frames.Count > 0)
+        {
+            var lastFrame = frames.Last();
+
+            if (lastFrame.IsCompleted())
+            {
+                var frame = new Frame(pins);
+                frames.Add(frame);
+            }
+            else
+            {
+                lastFrame.AddSecondRoll(pins);
+            }
+        }
+        else
+        {
+            var frame = new Frame(pins);
+            frames.Add(frame);
+        }
+
+        if (frames.Count - 1 > 0) {
+            if (frames[frames.Count-2].IsSpare() ) {
+                frames[frames.Count - 2].AddBonusRoll(pins);
+            }
+        }
+
+        if (frames.Count - 1 > 0)
+        {
+            if (frames[frames.Count - 2].IsStrike())
+            {
+                frames[frames.Count - 2].AddBonusRoll(pins);
+            }
+        }
+        if (frames.Count - 2 > 0)
+        {
+            if (frames[frames.Count - 3].IsStrike())
+            {
+                frames[frames.Count - 3].AddBonusRoll(pins);
+            }
+        }
+    }
+
+    public int Score()
+    {
+        return frames.Aggregate(0, (total, frame) => total + frame.Score());
+    }
+}
---
+++ BowlingTests/BowlingGameV2Test.cs
@@ -0,0 +1,118 @@
+using BowlingGameV2;
+
+namespace BowlingTests;
+
+public class BowlingGameV2Tests
+{
+    [Fact]
+    public void TestSingleRoll()
+    {
+        var bowling = new BowlingGame();
+        bowling.Roll(5);
+        Assert.Equal(5, bowling.Score());
+
+    }
+
+    [Fact]
+    public void TestTwoRolls() {
+        var bowling = new BowlingGame();
+        bowling.Roll(5);
+        bowling.Roll(3);
+        Assert.Equal(8, bowling.Score());
+    }
+
+    [Fact]
+   public void TestWithStrikeAndTwoRolls() {
+        var bowling = new BowlingGame();
+        bowling.Roll(10);
+        bowling.Roll(4);
+        bowling.Roll(3);
+        Assert.Equal(24, bowling.Score());
+    }
+
+   [Fact]
+   public void TestWithStrikeAndSingleRollFollowUp() {
+        var bowling = new BowlingGame();
+        bowling.Roll(10);
+        bowling.Roll(4);
+        Assert.Equal(18, bowling.Score());
+    }
+     
+    [Fact]
+   public void TestWithStrikeAndNoFollowUp() {
+        var bowling = new BowlingGame();
+        bowling.Roll(10);
+        Assert.Equal(10, bowling.Score());
+    }
+
+    [Fact]
+    public void TestWithSpareAndFollowUpRoll(){
+        var bowling = new BowlingGame();
+        bowling.Roll(4);
+        bowling.Roll(6);
+        bowling.Roll(3);
+        Assert.Equal(16, bowling.Score());
+    }
+    
+    [Fact]
+    public void TestWithSpareAndNoFollowUpRoll(){
+        var bowling = new BowlingGame();
+        bowling.Roll(4);
+        bowling.Roll(6);
+        Assert.Equal(10, bowling.Score());
+    }
+
+    [Fact]
+    public void TestFullGame() {
+        var bowling = new BowlingGame();
+        bowling.Roll(10);
+        bowling.Roll(4);
+        bowling.Roll(3);
+        bowling.Roll(8);
+        bowling.Roll(2);
+        bowling.Roll(10);
+        bowling.Roll(8);
+        bowling.Roll(1);
+        bowling.Roll(3);
+        bowling.Roll(3);
+        bowling.Roll(10);
+        bowling.Roll(10);
+        bowling.Roll(10);
+        bowling.Roll(6);
+        bowling.Roll(3);
+        Assert.Equal(162, bowling.Score());
+    }
+
+    [Fact]
+    public void TestWithAllStrikes() {
+        var bowling = new BowlingGame();
+        foreach (int v in Enumerable.Range(1,12)) {
+            bowling.Roll(10);
+        }
+        Assert.Equal(300, bowling.Score());
+    }
+
+    [Fact]
+    public void TestStrikesOnFinalFrame() {
+        var bowling = new BowlingGame();
+        foreach (int v in Enumerable.Range(1,18)) {
+            bowling.Roll(1);
+        }
+        bowling.Roll(10);
+        bowling.Roll(10);
+        bowling.Roll(10);
+        Assert.Equal(48, bowling.Score());
+    }
+
+    [Fact]
+    public void TestSpareOnFinalFrame() {
+        var bowling = new BowlingGame();
+        foreach (int v in Enumerable.Range(1,18)) {
+            bowling.Roll(1);
+        }
+        bowling.Roll(6);
+        bowling.Roll(4);
+        bowling.Roll(3);
+        Assert.Equal(31, bowling.Score());
+    }
+}
